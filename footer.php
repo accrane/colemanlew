0860452d1106b14df3727c65f3f42099e7bc144a
Austin Crane <austincrane@gmail.com>
2017-03-31 15:02:24 -0400

    new footer partnership

--- footer.php
+++ footer.php
@@ -16,8 +16,8 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="center"><?php get_template_part('template-parts/social-footer'); ?></div>
-		<div class="wrapper">
-			<div class="site-info">
+		
+			<div class="site-info  js-blocks">
 				<?php 
 				$sitemap = get_field('sitemap_link', 'option');
 				$address = get_field('address', 'option');
@@ -32,18 +32,30 @@
 					echo '<a href="'. get_bloginfo('url') . '/privacy-statement">Privacy Statement</a>';
 					echo ' // ';
 					echo '<a href="'. get_bloginfo('url') . '/legal-statement">Legal Statement</a>';
+					echo '<br>';
+					echo '<a href="'. $sitemap . '">Sitemap</a> // Site by <a target="_blank" href="http://bellaworksweb.com/?ref=colemanlew">Bellaworks</a>';
 				echo '</div><!-- footer left -->';
 
 				// Footer Left
-				echo '<div class="footer-right">';
-					echo '<a href="'. $sitemap . '">Sitemap</a> // Site by <a target="_blank" href="http://bellaworksweb.com/?ref=colemanlew">Bellaworks</a>';
-				echo '</div><!-- footer right -->';
+				// echo '<div class="footer-right">';
+					
+				// echo '</div><!-- footer right -->';
 				 
 				 ?>
 
+				 
+
 
 			</div><!-- .site-info -->
-	</div><!-- wrapper -->
+	
+	<div class="partnership js-blocks">
+	 	<a href="http://www.penrhyn.com/" target="_blank">
+	 		<h3>Gobal Strategic Partners</h3>
+	 		<div class="rollover">
+	 			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/partnership.png">
+	 		</div>
+	 	</a>
+	 
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 <?php the_field('google_analytics', 'option'); ?>
